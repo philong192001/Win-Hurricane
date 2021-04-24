a32d9ad713e84cf12cac453e2a5064b2a0d257fd
Tuan Pham <60849085+tuanpth1909@users.noreply.github.com>
2021-04-24 16:18:56 +0700

    new: page

---
+++ Controllers/FeedbackController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechWiz.WinHurricane.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
